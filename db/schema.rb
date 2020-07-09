2b3ab53058eedec78f06507f7cb522d88bf5d75e
ArafathN <nguyen.arafath@gmail.com>
2020-07-09 16:43:04 +0200

    Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_180356) do
+ActiveRecord::Schema.define(version: 2020_07_09_144104) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.integer "stars"
   end
 
 end
